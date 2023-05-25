80cf9e063313a7551e7c5e3b347a238c4618721f
NoranekoSan1000 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-05-25 17:50:12 +0900

    add ControllerInput

---
+++ Assets/VRControllerInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRControllerInput : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) Debug.Log("ROneTouch");
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) Debug.Log("RTwoTouch");
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch)) Debug.Log("LOneTouch");
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch)) Debug.Log("LTwoTouch");
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) Debug.Log("RIndexTrigger");
+        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger)) Debug.Log("LIndexTrigger");
+        if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger)) Debug.Log("RHandTrigger");
+        if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger)) Debug.Log("LHandTrigger");
+    }
+}
