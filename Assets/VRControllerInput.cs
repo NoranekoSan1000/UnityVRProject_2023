ac63943f50bd1e91350a7099348b519b1269336d
thousand0625 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-05-26 02:31:58 +0900

    update

--- Assets/VRControllerInput.cs
+++ Assets/VRControllerInput.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class VRControllerInput : MonoBehaviour
 {
+    private bool leftHanded;
 
     void Update()
     {
         if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) Debug.Log("ROneTouch");
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) Debug.Log("RTwoTouch");
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) Debug.Log("RIndexTrigger");
+        if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger)) Debug.Log("RHandTrigger");
+        
         if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch)) Debug.Log("LOneTouch");
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch)) Debug.Log("LTwoTouch");
-        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) Debug.Log("RIndexTrigger");
         if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger)) Debug.Log("LIndexTrigger");
-        if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger)) Debug.Log("RHandTrigger");
         if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger)) Debug.Log("LHandTrigger");
+        
     }
 }
