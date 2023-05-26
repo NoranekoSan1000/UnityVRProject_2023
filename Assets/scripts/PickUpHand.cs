6af425a7a880fd318978bce6b99e883d13d7f9c9
NoranekoSan1000 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-05-26 17:12:34 +0900

    feature PickUpGun

--- Assets/scripts/PickUpHand.cs
+++ Assets/scripts/PickUpHand.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PickUpHand : MonoBehaviour
 {
     [SerializeField] private HandStatus handStatus;
+
+    private GameObject KeepRightObject;
+    private GameObject KeepLeftObject;
+
     private void OnTriggerStay(Collider other)
     {
         Debug.Log(other.name);
@@ -14,14 +18,37 @@ public class PickUpHand : MonoBehaviour
             {
                 if (handStatus.RightHandStatus != "") return;
                 handStatus.setRightHand(other.name);
-                Destroy(other);
+                KeepRightObject = other.gameObject;
+                KeepRightObject.gameObject.SetActive(false);
             }
             if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
             {
                 if (handStatus.LeftHandStatus != "") return;
                 handStatus.setLeftHand(other.name);
-                Destroy(other);
+                KeepLeftObject = other.gameObject;
+                KeepLeftObject.gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            if (ReferenceEquals(KeepRightObject, null)) return;
+            KeepRightObject.transform.position = this.transform.position;
+            KeepRightObject.SetActive(true);
+            KeepRightObject = null;
+            handStatus.setRightHand("");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch))
+        {
+            if (ReferenceEquals(KeepLeftObject, null)) return;
+            KeepLeftObject.transform.position = this.transform.position;
+            KeepLeftObject.SetActive(true);
+            KeepLeftObject = null;
+            handStatus.setLeftHand("");
         }
     }
 }
