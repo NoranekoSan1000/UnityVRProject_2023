38d2186b7488fb0355f3ff29d59cc51564befe22
NoranekoSan1000 <88626463+NoranekoSan1000@users.noreply.github.com>
2023-06-09 16:34:28 +0900

    update

--- Assets/scripts/SetHands.cs
+++ Assets/scripts/SetHands.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class SetHands : MonoBehaviour
 {
+    [SerializeField] private GameObject armature;
+    [SerializeField] private GameObject Center;
 
-    [SerializeField] private GameObject CenterCamera;
-    [SerializeField] private GameObject RightController;
-    [SerializeField] private GameObject LeftControlelr;
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController characterController;
+
+    const float MOVE = 0.01f;
+
+    private void Start()
     {
-        Debug.Log("‚¸‚ê" + transform.position);
+        characterController = armature.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        var center = Center.transform.position;
+        var arm = armature.transform.position;
+        var ctrl = characterController.center;
+
+        Debug.Log(center.x + " " + arm.x);
+        Debug.Log(center.y + " " + arm.y);
+        Debug.Log(center.z + " " + arm.z);
+
+        ctrl.Set(ctrl.x, ctrl.y, ctrl.z);
+        
        
-        RightController.transform.parent = transform;
-        LeftControlelr.transform.parent = transform;
     }
 }
